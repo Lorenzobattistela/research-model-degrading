bd889fd76d6453060d5ad31b8e891f617a276ede
Lorenzobattistela <lorenzobattistela@gmail.com>
2023-07-21 09:02:13 -0300

    [feat] update script to save plots

--- plots.py
+++ plots.py
@@ -8,38 +8,36 @@ data = pd.read_csv("polynomial_regression_results.csv")
 # Calculate the time difference between training and prediction years
 data["time_difference"] = data["year-predicted"] - data["year-trained"]
 
-# Create scatter plots for performance differences
-fig, axes = plt.subplots(2, 2, figsize=(12, 8))
 
-
-# Function to plot performance difference vs. time difference
-def plot_performance_diff(data, metric, ax):
-    sns.scatterplot(data=data, x="time_difference", y=metric, ax=ax)
-    ax.set_xlabel("Time Difference (Years)")
-    ax.set_ylabel(f"{metric} Difference")
-    ax.axhline(y=0, color="red", linestyle="--")  # Reference line at y=0 (no change)
+# Function to plot performance difference vs. time difference and save the figure
+def plot_and_save_performance_diff(data, metric, title):
+    plt.figure(figsize=(8, 6))
+    sns.scatterplot(data=data, x="time_difference", y=metric)
+    plt.xlabel("Time Difference (Years)")
+    plt.ylabel(f"{metric} Difference")
+    plt.axhline(y=0, color="red", linestyle="--")  # Reference line at y=0 (no change)
+    plt.title(title)
+    plt.tight_layout()
+    plt.savefig(f"metrics_performance_{metric}_vs_year_difference.png")
+    plt.close()
 
 
 # Plot 1 - Mean Squared Error (MSE) Difference vs. Time Difference
-plot_performance_diff(data, "mse", ax=axes[0, 0])
+plot_and_save_performance_diff(
+    data, "mse", "Model Performance Difference - Mean Squared Error (MSE)"
+)
 
 # Plot 2 - R-squared (R2) Difference vs. Time Difference
-plot_performance_diff(data, "r2", ax=axes[0, 1])
+plot_and_save_performance_diff(
+    data, "r2", "Model Performance Difference - R-squared (R2)"
+)
 
 # Plot 3 - Mean Absolute Error (MAE) Difference vs. Time Difference
-plot_performance_diff(data, "mae", ax=axes[1, 0])
+plot_and_save_performance_diff(
+    data, "mae", "Model Performance Difference - Mean Absolute Error (MAE)"
+)
 
 # Plot 4 - Root Mean Squared Error (RMSE) Difference vs. Time Difference
-plot_performance_diff(data, "rmse", ax=axes[1, 1])
-
-# Add titles to the subplots
-axes[0, 0].set_title("Model Performance Difference - Mean Squared Error (MSE)")
-axes[0, 1].set_title("Model Performance Difference - R-squared (R2)")
-axes[1, 0].set_title("Model Performance Difference - Mean Absolute Error (MAE)")
-axes[1, 1].set_title("Model Performance Difference - Root Mean Squared Error (RMSE)")
-
-# Adjust layout to prevent overlapping of titles and labels
-plt.tight_layout()
-
-# Show the plots
-plt.show()
+plot_and_save_performance_diff(
+    data, "rmse", "Model Performance Difference - Root Mean Squared Error (RMSE)"
+)
